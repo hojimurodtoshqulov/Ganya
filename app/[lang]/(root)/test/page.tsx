90f79755abdb3247ba6dfff3c1d93e699560450d
BegzodAlimov <begzodalimov614@gmail.com>
2024-04-12 20:11:54 +0500

    img tegi Image ga o'zgartirildi

--- app/[lang]/(root)/test/page.tsx
+++ app/[lang]/(root)/test/page.tsx
@@ -2,13 +2,11 @@ import Info from "@/components/shared/info/info";
 const data = {
     title: "О нас",
     text: 'Платформа "Академия осознанного родительства" создана Ганей Усмановой – мамой, предпринимателем и инфлюенсером с реальным опытом воспитания двух детей. На платформе вы сможете получить знания и опыт, которые собраны не только на личном опыте воспитания двоих детей, но и подтверждены последними научными исследованиями.',
-    tags: ["инфлуэнсер", "бизнес-леди"],
-    url: "https://s3-alpha-sig.figma.com/img/db87/1723/da0ba68c8dbb932b6231c009477db70f?Expires=1713744000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Y1rZDKX45ptLSys5YWfGS73XjNBJwXpwlIEapaOOWyOrU9nJnx94Vv~wXdQnRiCgKxXiXnJZvH2GX~8Okug0CymPXASArCdRo3EsEx5efIHEQGaOLeRZXidGF7LelcHijLUmg6vZCTUA2ys0V1K6w~V5hfmN7momQVOLZBdI3fjPQTlNd67fbT4MybI0QsjSCopcOJGBG-spoTq~oUYYuefOG4wv~aacILE9fKwMPeM0cdmIaoXm2YteeS0Z7G5IGg0J3bJVkHWmDgPiTI8N-O7~C2CWzwM8J5FGU6mxWwWhfcq-NrCabpFwkKufeStxGhuA8OSQi5g3rvHrGa6QCg__"
+    tags: ["инфлуэнсер", "бизнес-леди"]
 }
 const data1 = {
     title: 'Курс “Прикорм без проблем”',
-    text: 'В разработке курса "Прикорм без проблем" я применила не только свой опыт, но и мировые научные подходы, обращаясь к знаниям и методикам, проверенным временем и научным сообществом. Для создания курса были привлечены выдающиеся эксперты в области ухода за детьми и педиатрии, каждый из которых внес свой вклад в формирование курса, чтобы предоставить вам наиболее ценные и актуальные знания. Это стало возможным благодаря моей вере в то, что гармоничное развитие ребенка начинается с осознанного подхода к прикорму, подкрепленного научными данными и лучшими практиками.',
-    url: "https://s3-alpha-sig.figma.com/img/db87/1723/da0ba68c8dbb932b6231c009477db70f?Expires=1713744000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Y1rZDKX45ptLSys5YWfGS73XjNBJwXpwlIEapaOOWyOrU9nJnx94Vv~wXdQnRiCgKxXiXnJZvH2GX~8Okug0CymPXASArCdRo3EsEx5efIHEQGaOLeRZXidGF7LelcHijLUmg6vZCTUA2ys0V1K6w~V5hfmN7momQVOLZBdI3fjPQTlNd67fbT4MybI0QsjSCopcOJGBG-spoTq~oUYYuefOG4wv~aacILE9fKwMPeM0cdmIaoXm2YteeS0Z7G5IGg0J3bJVkHWmDgPiTI8N-O7~C2CWzwM8J5FGU6mxWwWhfcq-NrCabpFwkKufeStxGhuA8OSQi5g3rvHrGa6QCg__"
+    text: 'В разработке курса "Прикорм без проблем" я применила не только свой опыт, но и мировые научные подходы, обращаясь к знаниям и методикам, проверенным временем и научным сообществом. Для создания курса были привлечены выдающиеся эксперты в области ухода за детьми и педиатрии, каждый из которых внес свой вклад в формирование курса, чтобы предоставить вам наиболее ценные и актуальные знания. Это стало возможным благодаря моей вере в то, что гармоничное развитие ребенка начинается с осознанного подхода к прикорму, подкрепленного научными данными и лучшими практиками.'
 }
 
 function Test() {
--- components/shared/info/info.tsx
+++ components/shared/info/info.tsx
@@ -1,31 +1,39 @@
 import Image from "next/image";
-
+import userImage from "@/images/IMG_0574 1.jpg";
+import { Comfortaa, Roboto } from 'next/font/google'
+const comfortaa = Comfortaa({
+    subsets: ['latin'],
+    display: 'swap',
+})
+const roboto = Roboto({
+    weight:'400',
+    subsets: ['latin'],
+    display: 'swap',
+})
 interface InfoProps {
     title: string;
     text: string;
     tags?: string[];
-    url: string;
     sort?: boolean;
 }
 
 function Info(props: InfoProps): JSX.Element {
     return <div className={`flex ${props.sort ? 'flex-row-reverse' : 'flex-row'} justify-between gap-8 w-full mx-auto px-6 mb-10  max-h-[650px]`}>
-        <div className=" flex flex-col justify-between flex-3 bg-main-100 max-w-[760px] px-10 py-[60px] rounded-[40px]">
+        <div className=" flex flex-col justify-between flex-3 gap-4 bg-main-100 max-w-[760px] px-10 py-[60px] rounded-[40px]">
             <div className="flex flex-col gap-4">
-                <h2 className="text-h2 leading-14">{props.title}</h2>
-                <p className="text-[22px] leading-8 text-main-200">{props.text}</p>
+                <h2 className={`text-h2  leading-[56px]  ${comfortaa.className}`}>{props.title}</h2>
+                <p className={`text-[22px] leading-8 text-main-200 ${roboto.className}`}>{props.text}</p>
             </div>
-            <p className="flex gap-2.5 flex-wrap">{props.tags && props.tags.map((tag: string, indx: number) => (
+            {props.tags &&
+                (<p className="flex gap-2.5 flex-wrap">{props.tags.map((tag: string, indx: number) => (
                 <span key={indx} className="px-4 py-2 border rounded-[30px] text-[22px] leading-8 border-main-300">{tag}</span>
-            ))}</p>
+            ))}</p>)}
         </div>
         <div className="max-w-[536px] flex-2">
-            {/* <img src={props.url} alt="user" className="w-full h-full object-cover rounded-[40px]" /> */}
             <Image
-                src="@/image/user.jfif"
-                width={500}
-                height={500}
-                sizes="100% , 100%"
+                className="w-full h-full rounded-[40px]"
+                priority
+                src={userImage}
                 alt="Picture of the author"
             />
         </div>
