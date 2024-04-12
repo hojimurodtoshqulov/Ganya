73d639c998255be1ec33f7dab361cea03dfba1d0
BegzodAlimov <begzodalimov614@gmail.com>
2024-04-12 17:28:41 +0500

    sasc

--- components/shared/info/info.tsx
+++ components/shared/info/info.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface InfoProps {
     title: string;
     text: string;
@@ -7,8 +9,8 @@ interface InfoProps {
 }
 
 function Info(props: InfoProps): JSX.Element {
-    return <div className={`flex ${props.sort ? 'flex-row-reverse' : 'flex-row'} justify-between gap-8 w-full mx-auto px-6 mb-10`}>
-        <div className=" flex flex-col justify-between flex-3 bg-main-100 max-w-[760px] max-h-[650px] px-10 py-[60px] rounded-[40px]">
+    return <div className={`flex ${props.sort ? 'flex-row-reverse' : 'flex-row'} justify-between gap-8 w-full mx-auto px-6 mb-10  max-h-[650px]`}>
+        <div className=" flex flex-col justify-between flex-3 bg-main-100 max-w-[760px] px-10 py-[60px] rounded-[40px]">
             <div className="flex flex-col gap-4">
                 <h2 className="text-h2 leading-14">{props.title}</h2>
                 <p className="text-[22px] leading-8 text-main-200">{props.text}</p>
@@ -17,8 +19,15 @@ function Info(props: InfoProps): JSX.Element {
                 <span key={indx} className="px-4 py-2 border rounded-[30px] text-[22px] leading-8 border-main-300">{tag}</span>
             ))}</p>
         </div>
-        <div className="max-w-[536px] flex-2 max-h-[650px]">
-            <img src={props.url} alt="user" className="w-full h-full object-cover rounded-[40px]" />
+        <div className="max-w-[536px] flex-2">
+            {/* <img src={props.url} alt="user" className="w-full h-full object-cover rounded-[40px]" /> */}
+            <Image
+                src="@/image/user.jfif"
+                width={500}
+                height={500}
+                sizes="100% , 100%"
+                alt="Picture of the author"
+            />
         </div>
     </div>;
 }
