bb77e5d895a5a15cd6fc94185fe40e4b761b2a83
BegzodAlimov <begzodalimov614@gmail.com>
2024-04-12 12:36:07 +0500

    stillar to'grilandi

--- components/shared/info/info.tsx
+++ components/shared/info/info.tsx
@@ -8,13 +8,13 @@ interface InfoProps {
 
 function Info(props: InfoProps): JSX.Element {
     return <div className={`flex ${props.sort ? 'flex-row-reverse' : 'flex-row'} justify-between gap-8 w-full mx-auto px-6 mb-10`}>
-        <div className=" flex flex-col justify-between flex-3 bg-main-100 max-w-[760px] max-h-[650px] px-[40px] py-[60px] rounded-[40px]">
+        <div className=" flex flex-col justify-between flex-3 bg-main-100 max-w-[760px] max-h-[650px] px-10 py-[60px] rounded-[40px]">
             <div className="flex flex-col gap-4">
-                <h2 className="text-h2 leading-[56px]">{props.title}</h2>
-                <p className="text-[22px] leading-[31.9px] text-main-200">{props.text}</p>
+                <h2 className="text-h2 leading-14">{props.title}</h2>
+                <p className="text-[22px] leading-8 text-main-200">{props.text}</p>
             </div>
-            <p className="flex gap-[10px] flex-wrap">{props.tags && props.tags.map((tag: string, indx: number) => (
-                <span key={indx} className="px-4 py-2 border-[1px] rounded-[30px] text-[22px] leading-[31.9px] border-main-300">{tag}</span>
+            <p className="flex gap-2.5 flex-wrap">{props.tags && props.tags.map((tag: string, indx: number) => (
+                <span key={indx} className="px-4 py-2 border rounded-[30px] text-[22px] leading-8 border-main-300">{tag}</span>
             ))}</p>
         </div>
         <div className="max-w-[536px] flex-2 max-h-[650px]">
