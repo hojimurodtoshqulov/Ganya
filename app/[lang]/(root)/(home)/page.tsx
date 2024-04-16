72f650aab15c3343b179edfbc0021eb712962504
akmal-solakhiddinov1 <104005218+akmal-solakhiddinov@users.noreply.github.com>
2024-04-16 21:53:13 +0500

    cource card component

--- app/[lang]/(root)/(home)/page.tsx
+++ app/[lang]/(root)/(home)/page.tsx
@@ -1,7 +1,45 @@
+import CourceCard from "@/components/shared/cource-card/courceCard";
 import Showcase from "@/components/shared/showcase";
 import SubscribtionForm from "@/components/shared/subscribtion-form/subscribtionForm";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { courceCardData } from "@/constants";
+
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
+export function AccordionDemo() {
+  return (
+    <Accordion type="single" collapsible className="w-full">
+      <AccordionItem value="item-1">
+        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It adheres to the WAI-ARIA design pattern.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-2">
+        <AccordionTrigger>Is it styled?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It comes with default styles that matches the other
+          components&apos; aesthetic.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-3">
+        <AccordionTrigger>Is it animated?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It&apos;s animated by default, but you can disable it if you
+          prefer.
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  )
+}
+
 
 export default function Home() {
   return (
@@ -9,6 +47,15 @@ export default function Home() {
       <Showcase />
 
       <SubscribtionForm />
+
+
+
+
+
+      <Accordion type="single" collapsible>
+        <CourceCard courceCardData={courceCardData} index="item-1" />
+      </Accordion>
+
 
       <h2 className="text-3xl text-main-300 font-bold">Template Buttons</h2>
       <div className="flex flex-col justify-center gap-5">
--- constants/index.tsx
+++ constants/index.tsx
@@ -25,3 +25,93 @@ export const navlink = [
     path: "contacts",
   },
 ];
+
+
+
+
+
+export const courceCardData = {
+  id: 1,
+  title: 'Прикорм без проблем',
+  image: './public/images/sibling-hero.png',
+  moduls: [
+    {
+      id: 'm1',
+      modulTitle: 'Модуль 1',
+      modulDescription: 'Приглашает вас погрузиться в тему введения первого прикорма для вашего малыша и узнать основные принципы BLW (Baby-Led Weaning – введение прикорма под руководством ребенка)',
+      videoLessons: [
+        { id: 'v1', video: 'https://video.com', title: 'video title' },
+        { id: 'v2', video: 'https://video.com', title: 'video title' },
+        { id: 'v3', video: 'https://video.com', title: 'video title' },
+        { id: 'v4', video: 'https://video.com', title: 'video title' },
+        { id: 'v5', video: 'https://video.com', title: 'video title' },
+        { id: 'v6', video: 'https://video.com', title: 'video title' }
+      ],
+    },
+    {
+      id: 'm2',
+      modulTitle: 'Модуль 1',
+      modulDescription: 'Приглашает вас погрузиться в тему введения первого прикорма для вашего малыша и узнать основные принципы BLW (Baby-Led Weaning – введение прикорма под руководством ребенка)',
+      videoLessons: [
+        { id: 'v1', video: 'https://video.com', title: 'video title' },
+        { id: 'v2', video: 'https://video.com', title: 'video title' },
+        { id: 'v3', video: 'https://video.com', title: 'video title' },
+        { id: 'v4', video: 'https://video.com', title: 'video title' },
+        { id: 'v5', video: 'https://video.com', title: 'video title' },
+        { id: 'v6', video: 'https://video.com', title: 'video title' }
+      ],
+    },
+    {
+      id: 'm3',
+      modulTitle: 'Модуль 1',
+      modulDescription: 'Приглашает вас погрузиться в тему введения первого прикорма для вашего малыша и узнать основные принципы BLW (Baby-Led Weaning – введение прикорма под руководством ребенка)',
+      videoLessons: [
+        { id: 'v1', video: 'https://video.com', title: 'video title' },
+        { id: 'v2', video: 'https://video.com', title: 'video title' },
+        { id: 'v3', video: 'https://video.com', title: 'video title' },
+        { id: 'v4', video: 'https://video.com', title: 'video title' },
+        { id: 'v5', video: 'https://video.com', title: 'video title' },
+        { id: 'v6', video: 'https://video.com', title: 'video title' }
+      ],
+    },
+    {
+      id: 'm4',
+      modulTitle: 'Модуль 1',
+      modulDescription: 'Приглашает вас погрузиться в тему введения первого прикорма для вашего малыша и узнать основные принципы BLW (Baby-Led Weaning – введение прикорма под руководством ребенка)',
+      videoLessons: [
+        { id: 'v1', video: 'https://video.com', title: 'video title' },
+        { id: 'v2', video: 'https://video.com', title: 'video title' },
+        { id: 'v3', video: 'https://video.com', title: 'video title' },
+        { id: 'v4', video: 'https://video.com', title: 'video title' },
+        { id: 'v5', video: 'https://video.com', title: 'video title' },
+        { id: 'v6', video: 'https://video.com', title: 'video title' }
+      ],
+    },
+    {
+      id: 'm5',
+      modulTitle: 'Модуль 1',
+      modulDescription: 'Приглашает вас погрузиться в тему введения первого прикорма для вашего малыша и узнать основные принципы BLW (Baby-Led Weaning – введение прикорма под руководством ребенка)',
+      videoLessons: [
+        { id: 'v1', video: 'https://video.com', title: 'video title' },
+        { id: 'v2', video: 'https://video.com', title: 'video title' },
+        { id: 'v3', video: 'https://video.com', title: 'video title' },
+        { id: 'v4', video: 'https://video.com', title: 'video title' },
+        { id: 'v5', video: 'https://video.com', title: 'video title' },
+        { id: 'v6', video: 'https://video.com', title: 'video title' }
+      ],
+    },
+    {
+      id: 'm6',
+      modulTitle: 'Модуль 1',
+      modulDescription: 'Приглашает вас погрузиться в тему введения первого прикорма для вашего малыша и узнать основные принципы BLW (Baby-Led Weaning – введение прикорма под руководством ребенка)',
+      videoLessons: [
+        { id: 'v1', video: 'https://video.com', title: 'video title' },
+        { id: 'v2', video: 'https://video.com', title: 'video title' },
+        { id: 'v3', video: 'https://video.com', title: 'video title' },
+        { id: 'v4', video: 'https://video.com', title: 'video title' },
+        { id: 'v5', video: 'https://video.com', title: 'video title' },
+        { id: 'v6', video: 'https://video.com', title: 'video title' }
+      ],
+    }
+  ]
+}
