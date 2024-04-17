8766b6bde038f044904a3d5098686764e96b08a0
bakhodir <bahodirabdullayev0556@gmail.com>
2024-04-17 13:17:25 +0500

    nimadir

--- components/shared/stati/stati.tsx
+++ components/shared/stati/stati.tsx
@@ -6,7 +6,7 @@ function Stati() {
   return (
     <div className="bg-csneutral-100 py-20 my-16">
       <div className="container">
-        <h2 className="title text-h2 leading-[56px] mb-8">Тарифы</h2>
+        <h2 className="title text-h2 leading-[56px] mb-8">Статьи</h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           <Card
             title="Роды в Lapino. Как это было?"
