f2fd02168b97c0ffa9445f8ab43d9f34f5f3fde2
Biloliddin_dev <bilol9876@gmail.com>
2024-05-05 19:55:10 +0500

    add articls page

--- app/[lang]/dashboard/admin/articles/page.tsx
+++ app/[lang]/dashboard/admin/articles/page.tsx
@@ -1,7 +1,38 @@
+import CardArticls from "@/components/dashboard/articls/articlscard";
+import CardStatya from "@/components/shared/stati/card-stati";
+import { Button } from "@/components/ui/button";
+import Heading from "@/components/ui/heading";
+import { Input } from "@/components/ui/input";
+import { Search } from "lucide-react";
+import Link from "next/link";
 import { FC } from "react";
 
 const Articles: FC = (): JSX.Element => {
-  return <div>page for Articles</div>;
+  const facedate = [1, 2, 3, 4, 5, 6, 7];
+  return (
+    <div>
+      <div className="flex items-center justify-between">
+        <div className="relative w-[350px] ">
+          <Search className="absolute top-[10px] left-2" />
+          <Input placeholder="Поиск статьей" className="w-full pl-[35px]" />
+        </div>
+        <Button variant={"main"}>{"Yangi maqola qo'shing"}</Button>
+      </div>
+      <div className="grid lg:grid-cols-4 gap-6 mt-10 md:grid-cols-2 grid-cols-1">
+        {facedate.map((e, i) => (
+          <Link key={i} href={`/dashboard/admin/articles/${i}`}>
+            <CardArticls
+              title="Роды в Lapino. Как это было?"
+              text="Описание"
+              time="Mar 25, 2024"
+              minut={9}
+              width
+            />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Articles;
--- components/dashboard/articls/articlscard.tsx
+++ components/dashboard/articls/articlscard.tsx
@@ -9,16 +9,16 @@ interface CardProps {
   hight?: boolean;
 }
 
-function CardStatya(props: CardProps) {
+function CardArticls(props: CardProps) {
   return (
     <div
-      className={`flex flex-wrap flex-col gap-4 rounded-[40px] p-6 ${!props.width ? "max-w-[424px]" : "w-full"} ${props.hight ? "h-[375px]" : "h-auto"} justify-between ${!props.bacraund ? "bg-white" : "bg-[#F4F5F5]"}`}
+      className={`flex flex-wrap flex-col gap-4 rounded-2xl p-6 ${!props.width ? "max-w-[424px]" : "w-full"} h-[200px]} justify-between ${!props.bacraund ? "bg-white" : "bg-[#F4F5F5]"}`}
     >
       <div className="flex flex-col gap-3">
-        <h2 className="text-[32px] leading-[44px] font-comfortaa text-csneutral-600">
+        <h2 className="text-[18px] leading-[28px] font-comfortaa text-csneutral-600">
           {props.title}
         </h2>
-        <p className="text-[22px] leading-[32px] text-csneutral-500">
+        <p className="text-[12px] leading-[18px] text-csneutral-500">
           {props.text}
         </p>
       </div>
@@ -31,4 +31,4 @@ function CardStatya(props: CardProps) {
   );
 }
 
-export default CardStatya;
+export default CardArticls;
