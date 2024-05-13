cef14652110b27dd4baa2353fbee47b17496f404
bakhodir <bahodirabdullayev0556@gmail.com>
2024-05-13 13:24:24 +0500

    team members

---
+++ constants/team.ts
@@ -0,0 +1,21 @@
+import Image1 from '@/images/team1.png'
+import Image2 from '@/images/team2.png'
+import Image3 from '@/images/team3.png'
+
+export const teamMembers = [
+  {
+    name: "Имя Фамилия",
+    job: "Основатель",
+    image: Image1
+  },
+  {
+    name: "Имя Фамилия",
+    job: "Преподаватель",
+    image: Image2
+  },
+  {
+    name: "Имя Фамилия",
+    job: "Куратор",
+    image: Image3
+  },
+]
