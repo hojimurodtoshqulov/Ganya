728af5d71d9d299fc58b520a24928db2c0315996
BegzodAlimov <begzodalimov614@gmail.com>
2024-05-19 19:57:39 +0500

    'add'

--- components/dashboard/kontent/card.tsx
+++ components/dashboard/kontent/card.tsx
@@ -12,17 +12,26 @@ interface banner {
     "link": string,
     "isPublished": boolean
 }
-const BannerCard = ({ banner, id, accessToken, lang }: { banner: banner, id: number, accessToken: string | undefined , lang:'uz' | 'ru'}) => {
+interface dictionary {
+    wep: string,
+    mobile: string,
+    text: string,
+    link: string,
+    btn: string,
+    save: string
+}
+const BannerCard = ({ banner, id, accessToken, lang, dictionary }: { banner: banner, id: number, accessToken: string | undefined , lang:'uz' | 'ru', dictionary: dictionary}) => {
     const [isOpen, setIsOpen] = useState(false)
     const [checked, setChecked] = useState(banner.isPublished)
     const onClick = () => {
         setIsOpen(!isOpen)
     }
+
     useEffect(() => {
         const updatedChecked = async () => {
             const formData = new FormData()
             formData.append('isPublished', String(checked));
-            await fetch(`https://oar-api.onrender.com/api/v1/banners/update/${banner.id}`, {
+             await fetch(`https://oar-api.onrender.com/api/v1/banners/update/${banner.id}`, {
                 method: "PATCH",
                 body: formData,
                 headers: {
@@ -39,7 +48,7 @@ const BannerCard = ({ banner, id, accessToken, lang }: { banner: banner, id: num
             <div className='rounded-2xl p-4 bg-white gap-3 flex flex-col w-[252px] h-[124px] relative'>
                 <div className='flex justify-between'>
                     <p className='text-neutral-500 text-lg'>{id + 1}-{lang === 'ru' ? "Баннер" : "Banner"}</p>
-                    <Switch className=' bg-main-300' defaultChecked={banner.isPublished ? true : false} onCheckedChange={() => {
+                    <Switch className=' bg-main-300' defaultChecked={checked} onCheckedChange={() => {
                         setChecked(prev => !prev)
                     }} />
                 </div>
@@ -47,7 +56,7 @@ const BannerCard = ({ banner, id, accessToken, lang }: { banner: banner, id: num
                 <p className='text-[22px] leading-[32px] text-neutral-500'></p>
                 <Button variant={"filled"} onClick={onClick} className='py-3 px-5'>Изменить</Button>
                 {isOpen ?
-                    (<Modal isOpen={isOpen} onClick={onClick} banner={banner} accessToken={accessToken} />) : ''}
+                    (<Modal isOpen={isOpen} onClick={onClick} banner={banner} accessToken={accessToken} dictionary={dictionary} />) : ''}
             </div>
         </>
 
