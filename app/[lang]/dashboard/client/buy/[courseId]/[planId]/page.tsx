d7a2f51e87acc59efe4eb95fd31225b1394b976f
Bakhodir Abdullaev <bahodirabdullayev0556@gmail.com>
2024-06-12 13:31:01 +0500

    anchor blank

--- app/[lang]/dashboard/client/buy/[courseId]/[planId]/page.tsx
+++ app/[lang]/dashboard/client/buy/[courseId]/[planId]/page.tsx
@@ -74,6 +74,7 @@ async function getData<T>(): Promise<T[] | Error> {
 export default async function BuyCourse({
   params: { lang, planId, courseId },
 }: Props) {
+  console.log("payment page refreshed");
   const data = await getData<FullCourses>();
 
   if (data instanceof Error) {
@@ -144,21 +145,21 @@ export default async function BuyCourse({
         </div>
         <div className="flex flex-col md:flex-row justify-center gap-4 items-center">
           <a
-            target="_blank"
+            // target="_blank"
             href={`https://my.click.uz/services/pay?service_id=33448&merchant_id=25047&amount=${plan?.discount ? plan?.discount : plan.price}&transaction_param=${planId}&additional_param3=${userId}`}
             className="border rounded-xl px-6 py-4 w-full flex justify-center md:w-[180px]"
           >
             <Image src={pay1} width={100} height={100} alt="Pay with Click" />
           </a>
           <a
-            target="_blank"
+            // target="_blank"
             href={`https://checkout.paycom.uz/${btoa(`m=6628f4dc2eb76ec81b6969eb;ac.user_id=${userId};ac.planId=${planId};a=${(plan?.discount ? plan?.discount : plan.price) * 100}`)}`}
             className="border rounded-xl px-6 py-4 w-full flex justify-center md:w-[180px]"
           >
             <Image src={pay2} width={100} height={100} alt="Pay with Payme" />
           </a>
           <a
-            target="_blank"
+            // target="_blank"
             href={`https://www.apelsin.uz/open-service?serviceId=498615742&planId=${planId}&userId=${userId}&amount=${(plan?.discount ? plan.discount : plan?.price) * 100}`}
             className="border rounded-xl px-6 py-4 w-full flex justify-center md:w-[180px]"
           >
