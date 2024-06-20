8a51d4449973ba7969898fb36197530c31d4d785
Bakhodir Abdullaev <bahodirabdullayev0556@gmail.com>
2024-06-20 13:20:46 +0500

    tabs

---
+++ app/[lang]/dashboard/admin/content/about.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const About: FC<{ lang: "uz" | "ru" }> = (): JSX.Element => {
+  return <div>About</div>;
+};
+
+export default About;
--- app/[lang]/dashboard/admin/content/page.tsx
+++ app/[lang]/dashboard/admin/content/page.tsx
@@ -2,26 +2,44 @@ import { FC } from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Banner from "@/components/dashboard/kontent";
 import Sharhlar from "@/components/dashboard/comments";
+import MainVideo from "./main-video";
+import About from "./about";
 
-const Content: FC<{ params: { lang: 'uz' | 'ru' } }> = ({
+const Content: FC<{ params: { lang: "uz" | "ru" } }> = ({
   params: { lang },
 }): JSX.Element => {
   return (
     <div>
       <h3 className="font-comfortaa font-bold text-[26px] text-main-300 capitalize">
-        {lang === "ru" ? "контент" : "Kontent" }
+        {lang === "ru" ? "контент" : "Kontent"}
       </h3>
 
       <Tabs defaultValue="banners">
         <TabsList className="mb-3 sm:mb-6">
-          <TabsTrigger value="banners">{lang === "ru" ? "Баннеры" : "Bannerlar"}</TabsTrigger>
-          <TabsTrigger value="fikrlar">{lang === "ru" ? "Отзывы" : "Sharhlar"}</TabsTrigger>
+          <TabsTrigger value="banners">
+            {lang === "ru" ? "Баннеры" : "Bannerlar"}
+          </TabsTrigger>
+          <TabsTrigger value="fikrlar">
+            {lang === "ru" ? "Отзывы" : "Sharhlar"}
+          </TabsTrigger>
+          <TabsTrigger value="main_video">
+            {lang === "ru" ? "Главное видео" : "Asosiy video"}
+          </TabsTrigger>
+          <TabsTrigger value="about">
+            {lang === "ru" ? "О нас" : "Biz haqimizda"}
+          </TabsTrigger>
         </TabsList>
         <TabsContent value="banners">
           <Banner lang={lang} />
         </TabsContent>
         <TabsContent value="fikrlar">
           <Sharhlar lang={lang} />
+        </TabsContent>
+        <TabsContent value="main_video">
+          <MainVideo lang={lang} />
+        </TabsContent>
+        <TabsContent value="about">
+          <About lang={lang} />
         </TabsContent>
       </Tabs>
     </div>
